6d0c74490bbbaa3a560de0314dd931d86f917978
orlane111 <nguessamiensa137@gmail.com>
2026-05-11 22:15:17 +0000

    import du scanner et generation du nombre aleatoire

--- src/Game.java
+++ src/Game.java
@@ -1,2 +1,13 @@
+import java.util.Scanner;
 public class Game {
+    public static void main(String[] args) {
+        // Création du lecteur pour l'entrée utilisateur
+        Scanner scanner = new Scanner(System.in);
+
+        // Génération d'un nombre entre 1 et 100
+        int nombreSecret = (int) (Math.random() * 100) + 1;
+
+        int essaisRestants = 10;
+        boolean aGagne = false;
+        System.out.println("Bienvenue ! Devine le nombre entre 1 et 100.");
 }
