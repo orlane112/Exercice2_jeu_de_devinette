cabf5a4be1d254f0af357da1706168926f968133
orlane111 <nguessamiensa137@gmail.com>
2026-05-11 22:17:36 +0000

    gestion des 10 essais et indices

--- src/Game.java
+++ src/Game.java
@@ -10,4 +10,20 @@ public class Game {
         int essaisRestants = 10;
         boolean aGagne = false;
         System.out.println("Bienvenue ! Devine le nombre entre 1 et 100.");
-}
+
+        //On utilise une boucle while qui tourne tant qu'on n'a pas gagné et qu'il reste des essais
+        while (essaisRestants > 0 && !aGagne) {
+            System.out.print("Il te reste " + essaisRestants + " essais. Ton choix : ");
+            int proposition = scanner.nextInt();
+            essaisRestants--; // On utilise un essai
+
+            if (proposition == nombreSecret) {
+                aGagne = true;
+            } else if (proposition < nombreSecret) {
+                System.out.println("C'est PLUS !");
+            } else {
+                System.out.println("C'est MOINS !");
+            }
+        }
+
+    }
